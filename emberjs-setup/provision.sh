25cdcdb02a2d0e8587d0dae2f040aa85a4195c07
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-01-26 00:22:08 -0700

    Configure Samba share.

--- emberjs-setup/provision.sh
+++ emberjs-setup/provision.sh
@@ -9,3 +9,24 @@ sudo apt-get install -y nodejs build-essential
 
 # Installing Git
 sudo apt-get install -y git
+
+# Installing Samba
+apt-get install -y samba samba-common python-glade2 system-config-samba
+
+# Setting up Samba
+share=/samba/ember-workspace
+mkdir -p $share
+chmod -R 0755 $share
+chown -R vagrant:vagrant $share
+
+mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
+mv /home/vagrant/smb.conf /etc/samba/smb.conf
+
+smb_password=vagrant
+(echo $smb_password; echo $smb_password) | smbpasswd -a vagrant
+
+service smbd restart
+
+# Setting up local workspace
+ln -s /samba/ember-workspace /home/vagrant/ember-workspace
+chown -h vagrant:vagrant /home/vagrant/ember-workspace
